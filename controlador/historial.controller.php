27a7fd81146082b4f4d696d94c70fc9f871ff40e
Jesus Martin Castro <jesmartin82@gmail.com>
2019-05-24 13:47:32 +0200

    Create historial.controller.php

---
+++ controlador/historial.controller.php
@@ -0,0 +1,69 @@
+<?php
+    require_once "modelo/usuario.php";
+
+    class controllerhistorial {
+        
+        public function __construct() {
+
+        }
+
+        public function search() {
+           $datos= historial::listado();
+           require_once "vista/historial.paciente.php";
+        }
+
+        public function crear(){
+            if(isset($_GET["iden"])):
+                $usuarios= new usuarios();
+                $usuarios->setdni($_GET["iden"]);
+                $usuarios->setnombre($_GET["nom"]);
+                $usuarios->setapellidos($_GET["apl"]);
+                $usuarios->setnacimiento($_GET["ncm"]);
+                $usuarios->agregar();
+                header("Location:index.php?mod=usuario&ope=index");
+            else:
+                require_once "vista/usuario.crear.php";
+            endif;
+        }
+
+        public function delete() {
+            if(isset($_GET["idUser"])) usuarios::deleteUsuarios($_GET["idUser"]);
+            header("Location:search.php?mod=usuario&ope=search");
+        }
+
+        public function update() {
+
+            $id = $_GET["idUser"]??"";
+
+            if(!empty($id)):
+                
+                $usuarios = usuarios::obtenerUsuario($id);
+                                    
+                if(isset($_GET["iden"])):
+
+                    $usuarios->setdni($_GET["iden"]);
+                    $usuarios->setnombre($_GET["nom"]);
+                    $usuarios->setapellidos($_GET["apl"]);
+                    $usuarios->setnacimiento($_GET["ncm"]);
+                    $usuarios->update();
+                    
+                    header("Location:index.php?mod=usuario&ope=index");
+                else:
+                    
+                    $dni = $usuarios->getdni() ;
+                    $nombre = $usuarios->getnombre() ;
+                    $apellidos = $usuarios->getapellidos() ;
+                    $nacimiento = $usuarios->getnacimiento() ;
+                    require_once "vista/usuario.update.php";
+                endif;
+            else:
+                header("Location:index.php?mod=usuario&ope=index");
+            endif;
+        }
+
+        public function usuarioConcreto() {
+            $datos= historia_clinico::listadovariado();
+            require_once "vista/usuario.informacion.php";
+        }
+
+    }
