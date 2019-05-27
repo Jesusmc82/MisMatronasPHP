281f7422b3b088eb5d0fe8ab881cfd580762bfc5
Jesus Martin Castro <jesmartin82@gmail.com>
2019-05-27 13:41:42 +0200

    Update acceso.menu.php

--- vista/acceso.menu.php
+++ vista/acceso.menu.php
@@ -6,9 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Usuario</title>
-
+    <link rel="icon" href="style/img/icon_Embarazada.ico">
+    <title>Mis Matronas</title>
     <!-- Bootstrap -->
     <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -17,55 +16,53 @@
     <link href="vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-    <!-- Datatables -->
-    <link href="vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <link href="vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-    <link href="vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-    <link href="vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-    <link href="vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+    <!-- bootstrap-wysiwyg -->
+    <link href="vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <!-- Select2 -->
+    <link href="vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <!-- Switchery -->
+    <link href="vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <!-- starrr -->
+    <link href="vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
     <!-- Custom Theme Style -->
     <link href="build/css/custom.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
   </head>
 
   <body class="nav-md">
+    <div class="containerDos">
     <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a class="site_title"><img src="style/img/icon_Embarazada.ico"><span> Mis Matronas</span></a>
+              <a class="site_title"><img src="style/img/icon_Embarazada.ico"> <span>Mis Matronas</span></a>
             </div>
 
             <div class="clearfix"></div>
 
-                <!-- /menu profile quick info -->
-
-            <!-- <br /> -->
-
-            <!-- sidebar menu -->
+            <br/>
+            <!-- menú lateral -->
+            
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div div="containerDos">  
               <div class="menu_section">
-                <h3>Directorio</h3>
+                
+                <h3>Menu</h3>
                 <ul class="nav side-menu">
-                  <li><a href="index.php?mod=acceso&ope=menu"><i class="fa fa-home"></i> Menu</a></li>
-                  <li><a href="index.php?mod=usuario&ope=search"><i class="fa fa-user"></i> Usuarios</a></li>
-                  <li><a><i class="fa fa-file-pdf-o"></i>  Historial </a></li>
-                  <li><a href="index.php?mod=usuario&ope=crear"><i class="fa fa-table"></i> Nuevo Usuario</a></li>
-                  <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation</a></li>
-                  <li><a><i class="fa fa-clone"></i>Layouts</a></li>
+                  <li><a><i class="fa fa-home"></i> Inicio</a></li>
+                  <li><a href="index.php?mod=usuario&ope=search"><i class="fa fa-user"></i> Usuario</a></li>
+                  <li><a><i class="fa fa-edit"></i> Nuevo Usuario</a></li>
+                  <li><a><i class="fa fa-file-word-o"></i> Historial</a></li>
+                  <li><a><i class="fa fa-calendar"></i> Citas</a></li>
+                  <li><a href="index.php?mod=acceso&ope=logout"><i class="fa fa-power-off"></i> Cierre de sesi&oacuten</a></li>
                 </ul>
-              </div>
+                </div>
+            </div> 
             </div>
-            <!-- /sidebar menu -->
-
-            <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="index.php?mod=acceso&ope=logout">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-            <!-- /menu footer buttons -->
           </div>
         </div>
 
@@ -76,18 +73,6 @@
               <div class="nav toggle">
                 <a id="menu_toggle"><i class="fa fa-bars"></i></a>
               </div>
-
-               <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
             </nav>
           </div>
         </div>
@@ -98,9 +83,10 @@
         <div class="right_col" role="main">
             <div class="clearfix"></div>
         
-            <h1>BIENVENIDO</h1>
+            <h1>BIENVENIDO <?php echo $_POST[`impresion`]; ?></h1>
 
-            <button type="button" class="btn btn-success btn-large"><a href="index.php?mod=usuario&ope=search"></i> Usuarios</a></button>
+            <!-- <button type="button" class="btn btn-success btn-large"><a href="index.php?mod=usuario&ope=search"></i> Usuarios</a></button> -->
+          </div>
     <!-- jQuery -->
     <script src="vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
