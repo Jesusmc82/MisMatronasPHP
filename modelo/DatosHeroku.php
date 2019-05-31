7333b08d9ca126a9b4ddd2192a56b351700f18d8
Jesus Martin Castro <jesmartin82@gmail.com>
2019-05-31 14:39:38 +0200

    pruebas

---
+++ modelo/DatosHeroku.php
@@ -0,0 +1,6 @@
+mysql://bffe6319859464:dbb3040b@eu-cdbr-west-02.cleardb.net/heroku_54bd8ecf8967fd9?reconnect=true
+phpmyadmin.co
+		Host = "eu-cdbr-west-02.cleardb.net";
+		User = "bffe6319859464";
+		Pass = "dbb3040b";
+		Name = "heroku_54bd8ecf8967fd9";
