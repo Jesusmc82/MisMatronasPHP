3be7c4aa52528ee040eb3af241a1874c66cea2f8
Jesus Martin Castro <jesmartin82@gmail.com>
2019-06-04 12:39:49 +0200

    Nueva Subida

--- modelo/historial.php
+++ modelo/historial.php
@@ -44,86 +44,104 @@
 
         }
 
-        public function historico() {
+        public function editarHistoria() {
             $db = Database::getInstance();
-            $db->query('SELECT * FROM historial_clinico;');
-            $datos=[];
-            while($item = $db->getRow('historial')):
-                array_push($datos,$item);
-            endwhile;
-            return $datos;
-            
+            $db->query('UPDATE historial SET nhc=:nhc , evolucion=:evl, idUser=:idU, observaciones=:obv WHERE idUser=:idU',
+                        [":nhc" => $this->nhc,
+                        ":evl" => $this->evolucion,
+                        ":idU" => $this->idUser,
+                        ":obv" => $this->observaciones]);
         }
 
-        public function agregar() {
+        public static function obtenerHistoria($id) {
             $db = Database::getInstance();
-            $db->query('INSERT INTO usuarios(nombre, apellidos, dni, nacimiento) VALUES (:nom, :apl, :iden, :ncm);',
-                        [":nom" =>$this->nombre,
-                         ":apl" =>$this->apellidos,
-                         ":iden" =>$this->dni,
-                         ":ncm" =>$this->nacimiento]);
-                    $this-> idUser - $db->getLastId();   
-        }
-
-        public function delete() {
-            $db = Database::getInstance();
-            $db->query('DELETE FROM usuarios WHERE idUser= :idU',
-                        [":idU" =>$this->idUser]);
-        }
-
-        public function deleteUsuarios($id) {
-            $db = Database::getInstance();
-            $db->query('DELETE FROM usuarios WHERE idUser = :idU',
-                        [":idU" => $id]);
-        }
-
-        // public function update() {
-        //     $db = Database::getInstance();
-        //     $db->query('UPDATE usuarios SET nombre=:nom , apellidos=:apl, dni=:iden, nacimiento=:ncm WHERE idUser=:idU',
-        //                 [":idU" => $this->idUser,
-        //                 ":nom" => $this->nombre,
-        //                 ":apl" => $this->apellidos,
-        //                 ":iden" => $this->dni,
-        //                 ":ncm" => $this->nacimiento]);
-        // }
-
-        public function listadovariado() {
-            $db = Database::getInstance();
-            $db->query('SELECT usuarios.nombre, historial_clinico.nhc, historial_clinico.evolucion, historial_clinico.observaciones FROM usuarios INNER JOIN historial_clinico ON historial_clinico.idUser=usuarios.idUser where usuarios.idUser=historial_clinico.idUser ;');
-            $datos=[];
-            while($item = $db->getRow('historial_clinico')):
-                array_push($datos,$item);
-            endwhile;
-            return $datos;
-        }
-
-        public static function obtenerUsuario($id) {
-            $db = Database::getInstance();
-            $db->query('SELECT * FROM usuarios WHERE idUser=1;',
+            $db->query('SELECT * FROM historial WHERE idUser=:idU;',
                         ["idU" => $id]);
 
             $res = $db->getRow("usuarios");
             return $res;
         }
 
-        public function __toString() {
-
-            return print_r($this, true) ;
-        }
-
-
-        // public function inicioSesion() {
-
+        // public function historico() {
+        //     $db = Database::getInstance();
+        //     $db->query('SELECT * FROM historial_clinico;');
+        //     $datos=[];
+        //     while($item = $db->getRow('historial')):
+        //         array_push($datos,$item);
+        //     endwhile;
+        //     return $datos;
+            
         // }
 
-        public function logout() {
-            session_start();
+        // public function agregar() {
+        //     $db = Database::getInstance();
+        //     $db->query('INSERT INTO usuarios(nombre, apellidos, dni, nacimiento) VALUES (:nom, :apl, :iden, :ncm);',
+        //                 [":nom" =>$this->nombre,
+        //                  ":apl" =>$this->apellidos,
+        //                  ":iden" =>$this->dni,
+        //                  ":ncm" =>$this->nacimiento]);
+        //             $this-> idUser - $db->getLastId();   
+        // }
 
-            $_SESSION=[];
+        // public function delete() {
+        //     $db = Database::getInstance();
+        //     $db->query('DELETE FROM usuarios WHERE idUser= :idU',
+        //                 [":idU" =>$this->idUser]);
+        // }
 
-            session_destroy();
+        // public function deleteUsuarios($id) {
+        //     $db = Database::getInstance();
+        //     $db->query('DELETE FROM usuarios WHERE idUser = :idU',
+        //                 [":idU" => $id]);
+        // }
 
-            // header("location:../vista/usuario.index.php");
-        }
+        // // public function update() {
+        // //     $db = Database::getInstance();
+        // //     $db->query('UPDATE usuarios SET nombre=:nom , apellidos=:apl, dni=:iden, nacimiento=:ncm WHERE idUser=:idU',
+        // //                 [":idU" => $this->idUser,
+        // //                 ":nom" => $this->nombre,
+        // //                 ":apl" => $this->apellidos,
+        // //                 ":iden" => $this->dni,
+        // //                 ":ncm" => $this->nacimiento]);
+        // // }
+
+        // public function listadovariado() {
+        //     $db = Database::getInstance();
+        //     $db->query('SELECT usuarios.nombre, historial_clinico.nhc, historial_clinico.evolucion, historial_clinico.observaciones FROM usuarios INNER JOIN historial_clinico ON historial_clinico.idUser=usuarios.idUser where usuarios.idUser=historial_clinico.idUser ;');
+        //     $datos=[];
+        //     while($item = $db->getRow('historial_clinico')):
+        //         array_push($datos,$item);
+        //     endwhile;
+        //     return $datos;
+        // }
+
+        // public static function obtenerUsuario($id) {
+        //     $db = Database::getInstance();
+        //     $db->query('SELECT * FROM usuarios WHERE idUser=1;',
+        //                 ["idU" => $id]);
+
+        //     $res = $db->getRow("usuarios");
+        //     return $res;
+        // }
+
+        // public function __toString() {
+
+        //     return print_r($this, true) ;
+        // }
+
+
+        // // public function inicioSesion() {
+
+        // // }
+
+        // public function logout() {
+        //     session_start();
+
+        //     $_SESSION=[];
+
+        //     session_destroy();
+
+        //     // header("location:../vista/usuario.index.php");
+        // }
 
     }
